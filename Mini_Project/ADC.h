8ed94f9b1facf0b151b132032c0ab66b5a072e5d
Annankun <124212659+Annankun@users.noreply.github.com>
2025-12-04 23:18:33 -0500

    Delete Mini_Project directory

--- Mini_Project/ADC.h
+++
@@ -1,9 +0,0 @@
-#ifndef ADC_H_
-#define ADC_H_
-
-#include <stdint.h>
-
-void ADC_Init(void);        // Initialize ADC for potentiometer
-uint8_t ADC_Read(void);     // Read potentiometer value (0-255)
-
-#endif /* ADC_H_ */
